0084d2496931f60ebc3f76ba69442f116fdd48b5
Fattahul Akbar <akbarfattahul22@gmail.com>
2023-11-06 18:25:54 +0700

    initial commit

---
+++ src/Applications/security/_test/EncryptionHelper.test.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+const PasswordHash = require('../PasswordHash');
+
+describe('EncryptionHelper interface', () => {
+  it('should throw error when invoke abstract behavior', async () => {
+    // Arrange
+    const passwordHash = new PasswordHash();
+
+    // Action and Assert
+    await expect(passwordHash.hash('dummy_password')).rejects.toThrowError('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
+    await expect(passwordHash.compare('plain', 'encrypted')).rejects.toThrowError('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED');
+  });
+});
